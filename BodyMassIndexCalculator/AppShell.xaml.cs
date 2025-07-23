1bd54c2e1443c6190bc50e61fb760a721bae32b8
Flakoooo <vlasov.vadom@gmail.com>
2025-07-23 16:48:38 +0500

    Add project files.

---
+++ BodyMassIndexCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BodyMassIndexCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
